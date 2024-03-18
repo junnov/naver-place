0167b7cee47d604d4b582acd072973bb5e480eda
Park JunYeong <firebolt21v@gmail.com>
2024-03-18 21:42:20 +0900

    style: apply nanumsquare to main font

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,6 @@ import NaverIcon from "@components/icons/NaverIcon.vue"
 <template>
   <div class="flex items-center justify-center gap-4 text-3xl font-bold">
     <NaverIcon class="h-12" />
-    Naver Place API
+    Naver Place API 네이버플레이스
   </div>
 </template>
