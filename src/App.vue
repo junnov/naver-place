4dea6386cd77babca381cbbe681e264f4b570cc4
Park JunYeong <firebolt21v@gmail.com>
2024-03-18 21:40:24 +0900

    feat: set alias

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import NaverIcon from "./components/icons/NaverIcon.vue"
+import NaverIcon from "@components/icons/NaverIcon.vue"
 </script>
 
 <template>
-  <div class="flex items-center justify-center gap-2 text-3xl font-bold">
+  <div class="flex items-center justify-center gap-4 text-3xl font-bold">
     <NaverIcon class="h-12" />
     Naver Place API
   </div>
